d50389b4592199cf562c36acf23c7a27f9a69220
Leonardo Patrocinio <93171296+ChicoRipCurl@users.noreply.github.com>
2025-06-03 16:07:30 -0500

    Components

---
+++ src/app/modules/prediccion/pages/demanda/demanda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demanda',
+  standalone: true,
+  imports: [],
+  templateUrl: './demanda.component.html',
+  styleUrl: './demanda.component.css'
+})
+export class DemandaComponent {
+
+}
---
+++ src/app/modules/registro/pages/compra-venta/compra-venta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compra-venta',
+  standalone: true,
+  imports: [],
+  templateUrl: './compra-venta.component.html',
+  styleUrl: './compra-venta.component.css'
+})
+export class CompraVentaComponent {
+
+}
